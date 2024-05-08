eb36a55353976f3f61465bdf096d84e6f59da13e
meha krimi <maha.krimi@esprit.tn>
2024-05-08 04:48:35 +0100

    v

--- src/main/java/com/example/mindmingle/controllers/CategoryEtudiantController.java
+++ src/main/java/com/example/mindmingle/controllers/CategoryEtudiantController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/categories-etudiant")
 public class CategoryEtudiantController {
